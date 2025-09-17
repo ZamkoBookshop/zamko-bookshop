f94f680ac9482d16d57bd279b662e3cf970a082f
ZamkoBookshop <jephersonlakai@gmail.com>
2025-09-17 20:12:12 +0100

    Delete Create a new file: components directory

--- Create a new file: components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-900 text-white text-center py-4">
-      <p>© {new Date().getFullYear()} Zamko Bookshop. All rights reserved.</p>
-    </footer>
-  );
-}
